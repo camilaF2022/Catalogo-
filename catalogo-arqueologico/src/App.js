496979928e90de2a052f7bad0f5bedd58a7c2d96
Isaias Venegas <isaias.venegas@ug.uchile.cl>
2024-05-07 19:21:50 -0400

    Define colors and typographies as shown in Figma

--- catalogo-arqueologico/src/App.js
+++ catalogo-arqueologico/src/App.js
@@ -1,29 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
+import theme from "./styles";
 import Button from "@mui/material/Button";
 import CollectionsBookmarkIcon from "@mui/icons-material/CollectionsBookmark";
+import { ThemeProvider } from "@mui/material/styles";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="contained" startIcon={<CollectionsBookmarkIcon />}>
-          Hello World
-        </Button>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Typography variant="h1">
+            Edit <code>src/App.js</code> and save to reload.
+          </Typography>
+          <Typography variant="p">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            suscipit, purus eget ultricies aliquam, purus sem ultrices augue,
+            nec laoreet nisi ipsum ac libero.
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<CollectionsBookmarkIcon />}
+          >
+            Hello World
+          </Button>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ catalogo-arqueologico/src/styles.js
@@ -0,0 +1,62 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { createTheme } from "@mui/material/styles";
+
+// Colors as shown in Figma
+const COLORS = {
+  MENU: "#56A0CC",
+  AZUL1: "#E8F1F2",
+  AZUL2: "#1B98E0",
+  AZUL3: "#006494",
+  AZUL4: "#13293D",
+};
+
+// Define theme colors and typography
+let theme = createTheme({
+  palette: {
+    menu: {
+      main: COLORS.MENU,
+    },
+    background: {
+      main: COLORS.AZUL1,
+    },
+    footer: {
+      main: COLORS.AZUL4,
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: 40,
+      fontWeight: 'bold',
+      color: 'black',
+    },
+    p: {
+      fontSize: 20,
+      fontWeight: 'normal',
+      color: 'black',
+    },
+  },
+});
+
+// Override primary and secondary colors
+// These colors have a main, light, dark and darker variants
+theme = createTheme(theme, {
+  palette: {
+    primary: theme.palette.augmentColor({
+      color: {
+        main: COLORS.AZUL2,
+      },
+      name: "primary",
+    }),
+    secondary: theme.palette.augmentColor({
+      color: {
+        main: COLORS.AZUL3,
+      },
+      name: "secondary",
+    }),
+  },
+});
+
+export default theme;
