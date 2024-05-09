b78c17c90c17acae085775861030effa93e2371a
Isaias Venegas <isaias.venegas@ug.uchile.cl>
2024-05-09 13:56:05 -0400

    Install react-router-dom and create a basic layout to route

--- catalogo-arqueologico/src/App.js
+++ catalogo-arqueologico/src/App.js
@@ -1,34 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Layout from "./components/Layout";
 import theme from "./styles";
-import Button from "@mui/material/Button";
-import CollectionsBookmarkIcon from "@mui/icons-material/CollectionsBookmark";
 import { ThemeProvider } from "@mui/material/styles";
-import { Typography } from "@mui/material";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Typography variant="h1">
-            Edit <code>src/App.js</code> and save to reload.
-          </Typography>
-          <Typography variant="p">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            suscipit, purus eget ultricies aliquam, purus sem ultrices augue,
-            nec laoreet nisi ipsum ac libero.
-          </Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            startIcon={<CollectionsBookmarkIcon />}
-          >
-            Hello World
-          </Button>
-        </header>
-      </div>
+      <Router>
+        <Layout />
+      </Router>
     </ThemeProvider>
   );
 }
