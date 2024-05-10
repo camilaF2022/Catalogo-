7e17a1ab49600a0fb948726fe3768716b00942eb
Bachis140315 <113462217+Bachis140315@users.noreply.github.com>
2024-05-10 11:59:14 -0300

    CRUD for Media table made, urls showed in metadata/docs

--- backend/catalogo_arqueologico/piezas/serializer.py
+++ backend/catalogo_arqueologico/piezas/serializer.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Metadata
+from .models import Metadata, Media
 
 class MetadataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Metadata
+        fields = '__all__'
+
+
+class MediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Media
         fields = '__all__'
