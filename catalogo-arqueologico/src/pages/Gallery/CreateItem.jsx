fcc95f4f1c72a362e957a566fe7421500732d54e
Isaias Venegas <isaias.venegas@ug.uchile.cl>
2024-05-11 16:13:03 -0400

    Implement user sessions using sessionStorage. Create private routes and NotFound page

---
+++ catalogo-arqueologico/src/pages/Gallery/CreateItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreateItem = () => {
+    return (
+        <div>
+            <h1>Welcome to the CreateItem page!</h1>
+            {/* Add your content here */}
+        </div>
+    );
+};
+
+export default CreateItem;
---
+++ catalogo-arqueologico/src/pages/Gallery/Gallery.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Gallery = () => {
+    return (
+        <div>
+            <h1>Welcome to the Gallery page!</h1>
+            {/* Add your content here */}
+        </div>
+    );
+};
+
+export default Gallery;
