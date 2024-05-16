c57c50d178942b5bee1577fe9070d38dca720c8b
Lukas <lvasquez@systep.cl>
2024-05-16 17:47:40 -0400

    removing remaining logs

--- catalogo-arqueologico/src/hooks/useToken.jsx
+++ catalogo-arqueologico/src/hooks/useToken.jsx
@@ -3,11 +3,11 @@ import { useState } from "react";
 export default function useToken() {
   const getToken = () => {
     const tokenString = sessionStorage.getItem("token");
+    // const userToken = JSON.parse(tokenString);
     return tokenString;
   };
 
   const [token, setToken] = useState(getToken());
-  console.log("Usetoken:token",token)
   const saveToken = (userToken) => {
     if (!!userToken) {
       // if token is not null, save it to sessionStorage
