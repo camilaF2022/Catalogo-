15b8d91b2aa689205a46eb5a40ed8e9cb9ac0d81
Bachis140315 <113462217+Bachis140315@users.noreply.github.com>
2024-05-23 10:15:41 -0300

    Url and root for uploading data

--- backend/catalogo_arqueologico/catalogo_arqueologico/settings.py
+++ backend/catalogo_arqueologico/catalogo_arqueologico/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,7 @@ CORS_ALLOWED_ORIGINS = []
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'piezas')
