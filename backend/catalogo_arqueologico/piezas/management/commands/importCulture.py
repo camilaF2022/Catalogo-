7ae79f1f7e750616302347163ce33153f87beb8e
Bachis140315 <113462217+Bachis140315@users.noreply.github.com>
2024-05-23 10:19:59 -0300

    Fill table Shape with the csv in catalogo_arqueoligico (where manage.py is)

--- backend/catalogo_arqueologico/piezas/management/commands/importCulture.py
+++ backend/catalogo_arqueologico/piezas/management/commands/importCulture.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandParser
 from django.db import IntegrityError
-from piezas.models import Metadata
+from piezas.models import Culture, CultureIds
 import csv
 
 class Command(BaseCommand):
@@ -15,11 +15,17 @@ class Command(BaseCommand):
             lector_csv = csv.reader(archivo_csv, delimiter=',')
             for fila in lector_csv:
                 try:
-                    Metadata.objects.create(
-                        type=2,
+                    Culture.objects.create(
                         name=fila[1]
                     )
                 except IntegrityError:
                     print("Error: No se puede insertar un objeto con el mismo valor para 'nombre'.")
                 
-                
+                recentlyAdded = Culture.objects.get(
+                    name=fila[1]
+                )
+                CultureIds.objects.create(
+                    culture=recentlyAdded.id,
+                    artifactid=int(fila[0])
+                )
+                print("todo funciona bien")
