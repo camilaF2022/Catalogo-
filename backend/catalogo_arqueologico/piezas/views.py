7eef4cab22c5e932d8f302ea96c15a8d639e107c
Bachis140315 <113462217+Bachis140315@users.noreply.github.com>
2024-05-24 15:23:29 -0300

    Views for list and create artifact and detail

--- backend/catalogo_arqueologico/piezas/views.py
+++ backend/catalogo_arqueologico/piezas/views.py
@@ -1,16 +1,24 @@
-from rest_framework import viewsets
-from .serializer import TagSerializer, ShapeSerializer, CultureSerializer
-from .models import Tag, Shape
+from rest_framework import viewsets, generics, permissions, authentication
+from .serializers import TagSerializer, ShapeSerializer, CultureSerializer, ArtifactSerializer
+from .models import Tag, Shape, Artifact
+from .authentication import TokenAuthentication
+from .permissions import IsFuncionarioPermission
+class ArtifactDetailAPIView(generics.RetrieveAPIView):
+    queryset = Artifact.objects.all()
+    serializer_class = ArtifactSerializer
+    #aca entregar todo, las urls y todo
 
+class ArtifactListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Artifact.objects.all()
+    serializer_class = ArtifactSerializer
+    authentication_classes = [authentication.SessionAuthentication, TokenAuthentication]
+    permission_classes = [permissions.IsAdminUser, IsFuncionarioPermission]
+    #En el list, se deberia entregar por lo menos el thumbnail
 
-# Create your views here.
-class MetadataView(viewsets.ModelViewSet):
-    serializer_class = TagSerializer
-    queryset = Tag.objects.all()
-
-class MediaView(viewsets.ModelViewSet):
-    serializer_class = ShapeSerializer
-    queryset = Shape.objects.all()
+class ArtifactDestroyAPIView(generics.DestroyAPIView):
+    queryset = Artifact.objects.all()
+    serializer_class = ArtifactSerializer
+    lookup_field = 'pk'
 
 
 #class MediaView(viewsets.ModelViewSet):
