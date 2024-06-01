8f5f999f8bf3951c109bce2799aa3882367435aa
Katia Fredes <katia.fredes.a@gmail.com>
2024-06-01 19:45:56 -0400

    refact: apply url's naming conventions

--- backend/catalogo_arqueologico/piezas/urls.py
+++ backend/catalogo_arqueologico/piezas/urls.py
@@ -12,9 +12,9 @@ from piezas import views
 
 urlpatterns = [
     path('docs/', include_docs_urls(title="Metadata API")),
-    path('oneArtifact/<int:pk>/delete', views.ArtifactDestroyAPIView.as_view()),
-    path('oneArtifact/<int:pk>/', views.ArtifactDetailAPIView.as_view()),
-    path('listArtifacts/', views.ArtifactListAPIView.as_view()),
-    path('create/', views.ArtifactCreateAPIView.as_view()),
-    path('catalog/', views.CatalogAPIView.as_view()),
+    path('', views.CatalogAPIView.as_view()),
+    path('artifact/<int:pk>/', views.ArtifactDetailAPIView.as_view()),
+    path('artifact/<int:pk>/delete', views.ArtifactDestroyAPIView.as_view()),
+    path('artifact/upload', views.ArtifactCreateAPIView.as_view()),
+    path('artifacts/', views.ArtifactListAPIView.as_view()),  # This is a temporal view to list all artifacts
 ]
