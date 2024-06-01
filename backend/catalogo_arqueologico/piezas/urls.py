ff60c5627d3418c96eabe40b007cf0058cd14bc1
Katia Fredes <katia.fredes.a@gmail.com>
2024-06-01 19:58:39 -0400

    docs: delete unnecesary commet

--- backend/catalogo_arqueologico/piezas/urls.py
+++ backend/catalogo_arqueologico/piezas/urls.py
@@ -3,13 +3,6 @@ from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
 from piezas import views
 
-
-#router = routers.DefaultRouter()
-#router.register(r'artifact', views.ArtifactView, 'Artifact')
-
-#router_media = routers.DefaultRouter()
-#router_media.register(r'tag', views.TagView, 'Tag')
-
 urlpatterns = [
     path('docs/', include_docs_urls(title="Metadata API")),
     path('', views.CatalogAPIView.as_view()),
