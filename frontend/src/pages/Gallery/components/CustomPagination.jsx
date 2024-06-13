dfe32ad4080f22315657f97de11c187fdcc409fc
Isaias Venegas <isaias.venegas@ug.uchile.cl>
2024-06-13 14:01:08 -0400

    Add current page to search params

--- frontend/src/pages/Gallery/components/CustomPagination.jsx
+++ frontend/src/pages/Gallery/components/CustomPagination.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Pagination } from "@mui/material";
+import { useSearchParams } from "react-router-dom";
 
 const CustomPagination = ({ pagination, setPagination }) => {
   const { currentPage, totalPages } = pagination;
 
+  // Search params from the URL
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // Avoid updating the URL when the component mounts and there are search params already
+  const [updateParamsFlag, setUpdateParamsFlag] = useState(false);
+
   const handlePageChange = (_, page) => {
     setPagination({ ...pagination, currentPage: page });
   };
+
+  // Initialize the pagination state with the search params
+  useEffect(() => {
+    const page = searchParams.get("page")
+      ? parseInt(decodeURIComponent(searchParams.get("page")))
+      : 1;
+    setPagination({ ...pagination, currentPage: page });
+    setUpdateParamsFlag(true);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // Update the URL when the current page changes
+  useEffect(() => {
+    if (!updateParamsFlag) {
+      return;
+    }
+    searchParams.set("page", currentPage);
+    setSearchParams(searchParams);
+  }, [currentPage]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Box display="flex" justifyContent="center" p={2}>
