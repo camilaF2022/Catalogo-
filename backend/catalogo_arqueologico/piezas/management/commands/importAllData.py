529022373ef9a110556cc0af4be42c88da8ef480
Isaias Venegas <isaias.venegas@ug.uchile.cl>
2024-06-16 21:11:10 -0400

    Use bulk_creation in culture and tags creation

--- backend/catalogo_arqueologico/piezas/management/commands/importAllData.py
+++ backend/catalogo_arqueologico/piezas/management/commands/importAllData.py
@@ -10,25 +10,27 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel("INFO")
 
+
 class Command(BaseCommand):
     help = "This command executes every other command related to imports. Doesn't need any arguments."
 
     def handle(self, *args, **kwargs):
 
-        #Get every shape txt file, to run importShape with that file
+        # Get every shape txt file, to run importShape with that file
         shapes_txt_folder = os.listdir(settings.SHAPE_FOLDER_PATH)
-        shapes_txt = [archivo for archivo in shapes_txt_folder if archivo.endswith('.txt')]
+        shapes_txt = [
+            archivo for archivo in shapes_txt_folder if archivo.endswith(".txt")
+        ]
 
-        management.call_command('importTags')
-        
+        management.call_command("importTags")
+
         for shape in shapes_txt:
-            management.call_command('importShape', shape)
+            management.call_command("importShape", shape)
 
-        management.call_command('importCulture')
+        management.call_command("importCulture")
 
-        management.call_command('importModel3D')
+        management.call_command("importModel3D")
 
-        management.call_command('importThumbs')
+        management.call_command("importThumbs")
 
-        management.call_command('importDescriptions')
-
+        management.call_command("importDescriptions")
