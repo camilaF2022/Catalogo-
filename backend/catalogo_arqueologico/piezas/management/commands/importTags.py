4a9322691e0ec8767e1721ecf30f0298f9691ef2
Isaias Venegas <38821553+IsaiasVenegas@users.noreply.github.com>
2024-06-26 14:59:40 -0400

    Load data optimization(#13)
    Use bulk_creation in culture and tags creation

--- backend/catalogo_arqueologico/piezas/management/commands/importTags.py
+++ backend/catalogo_arqueologico/piezas/management/commands/importTags.py
@@ -11,36 +11,60 @@ logger.setLevel("INFO")
 
 
 class Command(BaseCommand):
-    help = "Import tags from a CSV file. The CSV file must contain a list of ids and tags."
+    help = (
+        "Import tags from a CSV file. The CSV file must contain a list of ids and tags."
+    )
 
     def handle(self, *args, **kwargs):
         file = settings.TAGS_CSV_PATH
         if not os.path.exists(file):
             logger.error(f"File {file} not found. Stop")
             return
-        
+
+        tag_artifactIds = {}
+
         with open(file, newline="") as archivo_csv:
             artifact_tags_relationships = csv.reader(archivo_csv, delimiter=",")
+            # Fill the dictionary with the tags and the artifacts that have that tag
             for artifact_tags_tuple in artifact_tags_relationships:
+                artifactId = int(artifact_tags_tuple[0])
                 tags = artifact_tags_tuple[1].split(", ")
-                # Create tag object
                 for tag in tags:
-                    try:
-                        recentlyAdded = Tag.objects.create(name=tag)
-                    except IntegrityError:
-                        logger.info(f"Tag {tag} already exists. Skipping its creation")
-                        recentlyAdded = Tag.objects.get(name=tag)
+                    if tag not in tag_artifactIds:
+                        tag_artifactIds[tag] = [artifactId]
+                    else:
+                        tag_artifactIds[tag].append(artifactId)
+        archivo_csv.close()
 
-                    # Store relationship between tag and future artifact to be created
-                    try: 
-                        TagsIds.objects.create(
-                            tag=recentlyAdded.id, artifactid=int(artifact_tags_tuple[0])
-                        )
-                        logger.info(
-                            f"{tag}-{artifact_tags_tuple[0]} tagIds relationship added successfully"
-                        )
-                    except IntegrityError:
-                        logger.warning(
-                            f"{tag}-{artifact_tags_tuple[0]} tagIds relationship already exists. Skipping its creation"
-                        )
-                        continue
+        # Create tag objects
+        for tag in tag_artifactIds:
+            try:
+                recentlyAdded = Tag.objects.create(name=tag)
+            except IntegrityError:
+                logger.info(f"Tag {tag} already exists. Skipping its creation")
+                recentlyAdded = Tag.objects.get(name=tag)
+            # Store relationship between tag and future artifact to be created
+            relationships_previous_count = TagsIds.objects.filter().count()
+            try:
+                tag_ids_objects = [
+                    TagsIds(tag=recentlyAdded.id, artifactid=id)
+                    for id in tag_artifactIds[tag]
+                ]
+                TagsIds.objects.bulk_create(tag_ids_objects, ignore_conflicts=True)
+                relationships_current_count = TagsIds.objects.filter().count()
+                new_relationships_count = (
+                    relationships_current_count - relationships_previous_count
+                )
+                logger.info(
+                    f"{new_relationships_count} tagIds relationships for {tag} were successfully added"
+                )
+            except:
+                relationships_current_count = TagsIds.objects.filter().count()
+                new_relationships_count = (
+                    relationships_current_count - relationships_previous_count
+                )
+                logger.error(
+                    "Only {} tagIds relationships for {} could be created. Stop".format(
+                        new_relationships_count, tag
+                    )
+                )
