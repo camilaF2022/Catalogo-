6dbc2ae040b9dc2ebde4267ee2af662e1b4408b9
Ignacio Parada <Newton-Boy@users.noreply.github.com>
2024-06-29 00:45:37 -0400

    Autentificación con forms hecho

--- backend/catalogo_arqueologico/piezas/admin.py
+++ backend/catalogo_arqueologico/piezas/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import Tag, Shape, Culture, Artifact, CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
     model=CustomUser
     list_display=['username','email','role','institution','rut','is_staff','is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role', 'rut', 'institution')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('role', 'rut', 'institution')}),
+    )
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
@@ -31,6 +40,7 @@ class ArtifactAdmin(admin.ModelAdmin):
     search_fields = ("description",)
 
 
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Shape, ShapeAdmin)
 admin.site.register(Culture, CultureAdmin)
