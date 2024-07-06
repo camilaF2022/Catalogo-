f2a2bd2d6ebd491f696b0da379dad8bee2b2fe73
Katia Fredes <katia.fredes.a@gmail.com>
2024-07-05 20:00:31 -0400

    add: documentation in admin

--- backend/catalogo_arqueologico/piezas/admin.py
+++ backend/catalogo_arqueologico/piezas/admin.py
@@ -16,6 +16,18 @@ from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
 class CustomUserAdmin(UserAdmin):
+    """
+    Custom user model admin that represents the CustomUser model in the Django admin interface.
+    
+    Attributes:
+        add_form (forms.ModelForm): The form used for creating new users.
+        form (forms.ModelForm): The form used for changing user information.
+        model (models.Model): The CustomUser model.
+        list_display (list): A list of field names to display in the user list page.
+        list_filter (tuple): A tuple of field names to filter by in the user list page.
+        fieldsets (tuple): A tuple specifying the layout of the admin “change” page.
+        add_fieldsets (tuple): A tuple specifying the layout of the admin “add” page.
+    """
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
@@ -39,43 +51,101 @@ class CustomUserAdmin(UserAdmin):
     )
 
     def get_groups(self, obj):
+        """
+        Returns a comma-separated string of the user's groups.
+        
+        Parameters:
+            obj (CustomUser): The user instance.
+            
+        Returns:
+            str: A comma-separated string of group names.
+        """
         return ", ".join([group.name for group in obj.groups.all()])
 
     get_groups.short_description = "Groups"
 
     def save_model(self, request, obj, form, change):
+        """
+        Saves the model instance and updates the user's group memberships.
+        
+        Parameters:
+            request (HttpRequest): The HTTP request instance.
+            obj (CustomUser): The user instance being saved.
+            form (ModelForm): The form instance.
+            change (bool): True if this is a change operation, False if this is a creation operation.
+        """
         super().save_model(request, obj, form, change)
         obj.update_group()
 
     def response_change(self, request, obj):
+        """
+        Handles the response after a change has been made to a user instance.
+        
+        Parameters:
+            request (HttpRequest): The HTTP request instance.
+            obj (CustomUser): The user instance that was changed.
+            
+        Returns:
+            HttpResponse: The HTTP response to send back to the client.
+        """
         response = super().response_change(request, obj)
         obj.update_group()
         return response
 
 
 class TagAdmin(admin.ModelAdmin):
+    """
+    Admin interface options for Tag model.
+    
+    Attributes:
+        list_display (tuple): Fields to display in the admin list view.
+        list_filter (tuple): Fields to filter by in the admin list view.
+        search_fields (tuple): Fields to search in the admin list view.
+    """
     list_display = ("id", "name")
     list_filter = ("name",)
     search_fields = ("name",)
 
 
 class ShapeAdmin(admin.ModelAdmin):
+    """
+    Admin interface options for Shape model.
+    
+    Attributes:
+        list_display (tuple): Fields to display in the admin list view.
+        list_filter (tuple): Fields to filter by in the admin list view.
+        search_fields (tuple): Fields to search in the admin list view.
+    """
     list_display = ("id", "name")
     list_filter = ("name",)
     search_fields = ("name",)
 
-
 class CultureAdmin(admin.ModelAdmin):
+    """
+    Admin interface options for Culture model.
+    
+    Attributes:
+        list_display (tuple): Fields to display in the admin list view.
+        list_filter (tuple): Fields to filter by in the admin list view.
+        search_fields (tuple): Fields to search in the admin list view.
+    """
     list_display = ("id", "name")
     list_filter = ("name",)
     search_fields = ("name",)
 
 
 class ArtifactAdmin(admin.ModelAdmin):
+    """
+    Admin interface options for Artifact model.
+    
+    Attributes:
+        list_display (tuple): Fields to display in the admin list view.
+        list_filter (tuple): Fields to filter by in the admin list view.
+        search_fields (tuple): Fields to search in the admin list view.
+    """
     list_display = ("id", "description")
     list_filter = ("id",)
     search_fields = ("description",)
-
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Tag, TagAdmin)
