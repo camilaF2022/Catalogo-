3eb31ad92f637ee06e3ef57494f08e8b30d6ffcf
Katia Fredes <katia.fredes.a@gmail.com>
2024-07-09 19:53:40 -0400

    style: change formatting ' to " according to pylint

--- backend/catalogo_arqueologico/catalogo_arqueologico/urls.py
+++ backend/catalogo_arqueologico/catalogo_arqueologico/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 from catalogo_arqueologico.views import LoginView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', LoginView.as_view()),
-    path('catalog/', include('piezas.urls')),
+    path("admin/", admin.site.urls),
+    path("auth/", LoginView.as_view()),
+    path("catalog/", include("piezas.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
