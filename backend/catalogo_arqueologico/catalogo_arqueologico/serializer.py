7a1934aeea7e49695c0db4c914a6a7cb5a274911
Katia Fredes <katia.fredes.a@gmail.com>
2024-07-09 19:41:27 -0400

    style: fommat

--- backend/catalogo_arqueologico/catalogo_arqueologico/serializer.py
+++ backend/catalogo_arqueologico/catalogo_arqueologico/serializer.py
@@ -1,13 +1,13 @@
 from piezas.models import CustomUser
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
     full_name = serializers.SerializerMethodField()
-    
+
     def get_full_name(self, obj):
         return f"{obj.first_name} {obj.last_name}"
-    
+
     class Meta:
         model = CustomUser
         fields = ["id", "username", "email", "full_name"]
-
