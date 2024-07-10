81779481c9630af7d940a4cdf36a6571705b37e0
paulacalderona <70768936+paulacalderona@users.noreply.github.com>
2024-07-10 14:17:51 -0400

    Update ImageModal.jsx
    updating with documentation

--- frontend/src/pages/ArtifactDetails/components/ImageModal.jsx
+++ frontend/src/pages/ArtifactDetails/components/ImageModal.jsx
@@ -3,35 +3,55 @@ import { Modal, Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import CancelIcon from "@mui/icons-material/Cancel";
 
+/**
+ * ImageModal component displays an image in a modal.
+ * @param {string} path - The URL or path of the image to display.
+ */
 const ImageModal = ({ path }) => {
-  const [open, setOpen] = useState(false);
-  const [isWideImage, setIsWideImage] = useState(false);
+  // State variables
+  const [open, setOpen] = useState(false); // Controls modal open state
+  const [isWideImage, setIsWideImage] = useState(false); // Indicates if image is wide
+
+  // Opens the modal
   const handleOpen = () => setOpen(true);
+
+  // Closes the modal
   const handleClose = () => setOpen(false);
 
+  // Determines if the image is wide based on its dimensions
   const handleImageSize = (path) => {
     const img = new Image();
     img.src = path;
     img.onload = () => {
       if (img.width / img.height > 2) {
-        setIsWideImage(true);
+        setIsWideImage(true); // Sets isWideImage to true if image is wide
       } else {
-        setIsWideImage(false);
+        setIsWideImage(false); // Sets isWideImage to false if image is not wide
       }
     };
   };
+  
+   // Effect hook to handle changes in the path prop
   useEffect(() => {
-    handleImageSize(path);
+    handleImageSize(path); // Calls handleImageSize when path changes
   }, [path]);
+  
+  // Renders the component
   return (
     <div>
+      {/* Image displayed as a clickable thumbnail */}
       <CustomImage src={path} alt="lazy" onClick={handleOpen} />
+
+      {/* Modal dialog for displaying the image */}
       <CustomModal open={open} onClose={handleClose}>
         <CustomBox>
+          {/* Close icon */}
           <CancelIcon
             onClick={handleClose}
             style={{ cursor: "pointer", color: "white" }}
           />
+          
+          {/* Container for the image */}
           <ImageContainer
             iswideimage={isWideImage ? isWideImage.toString() : undefined}
           >
@@ -47,7 +67,9 @@ const ImageModal = ({ path }) => {
     </div>
   );
 };
+// Styled components
 
+// Styled image thumbnail
 const CustomImage = styled("img")(() => ({
   cursor: "pointer",
   width: "100%",
@@ -55,6 +77,7 @@ const CustomImage = styled("img")(() => ({
   objectFit: "cover",
 }));
 
+// Styled box container for modal content
 const CustomBox = styled(Box)(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
@@ -64,6 +87,7 @@ const CustomBox = styled(Box)(({ theme }) => ({
   padding: theme.spacing(1),
 }));
 
+// Styled modal dialog
 const CustomModal = styled(Modal)(() => ({
   display: "flex",
   flexDirection: "column",
@@ -71,6 +95,7 @@ const CustomModal = styled(Modal)(() => ({
   justifyContent: "center",
 }));
 
+// Styled container for the image within the modal
 const ImageContainer = styled(Box)(({ theme, iswideimage }) => ({
   padding: theme.spacing(4),
   display: "flex",
