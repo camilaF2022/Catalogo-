776c84a2523db5f8a85f49b7585421e0e2cd692d
Katia Fredes <katia.fredes.a@gmail.com>
2024-07-09 22:03:46 -0400

    style: change formatting ' to " according to black formatter

--- backend/catalogo_arqueologico/piezas/urls.py
+++ backend/catalogo_arqueologico/piezas/urls.py
@@ -4,13 +4,12 @@ from rest_framework import routers
 from piezas import views
 
 urlpatterns = [
-    path('docs/', include_docs_urls(title="Metadata API")),
-    path('artifacts/', views.CatalogAPIView.as_view()),
-    path('artifact/upload', views.ArtifactCreateUpdateAPIView.as_view()),
-    path('artifact/<int:pk>/', views.ArtifactDetailAPIView.as_view()),
-    path('artifact/<int:pk>/update', views.ArtifactCreateUpdateAPIView.as_view()),
-    path('metadata/', views.MetadataListAPIView.as_view()),
-    path('institutions/', views.InstitutionAPIView.as_view()),
-    path('artifact/<int:pk>/download', views.ArtifactDownloadAPIView.as_view()),
+    path("docs/", include_docs_urls(title="Metadata API")),
+    path("artifacts/", views.CatalogAPIView.as_view()),
+    path("artifact/upload", views.ArtifactCreateUpdateAPIView.as_view()),
+    path("artifact/<int:pk>/", views.ArtifactDetailAPIView.as_view()),
+    path("artifact/<int:pk>/update", views.ArtifactCreateUpdateAPIView.as_view()),
+    path("metadata/", views.MetadataListAPIView.as_view()),
+    path("institutions/", views.InstitutionAPIView.as_view()),
+    path("artifact/<int:pk>/download", views.ArtifactDownloadAPIView.as_view()),
 ]
-
