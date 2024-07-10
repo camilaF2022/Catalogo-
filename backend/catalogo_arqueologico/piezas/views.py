f94854f70699357c29fbe7d0159af787d43cfdcf
Katia Fredes <katia.fredes.a@gmail.com>
2024-07-09 22:38:33 -0400

    docs: add dosctring documentation of piezas' views

--- backend/catalogo_arqueologico/piezas/views.py
+++ backend/catalogo_arqueologico/piezas/views.py
@@ -1,3 +1,18 @@
+"""
+This module defines API views for handling operations related to Artifacts, Metadata, 
+and Artifact Downloads in the application.
+
+Classes:
+- ArtifactDetailAPIView: Provides a detail view for a single artifact. 
+- MetadataListAPIView: Provides a list view for metadata related to artifacts. 
+- ArtifactDownloadAPIView: Handles both the retrieval of detailed information about 
+    an artifact and the creation of artifact requester records. 
+- CustomPageNumberPagination: Provides paginated responses for API views.
+- CatalogAPIView: Provides a list view for artifacts in the catalog.
+- ArtifactCreateUpdateAPIView: Provides functionality for creating and updating artifacts.
+- InstitutionAPIView: Provides a list view for institutions.
+"""
+
 import os
 from django.conf import settings
 from .authentication import TokenAuthentication
@@ -41,15 +56,54 @@ logger = logging.getLogger(__name__)
 
 
 class ArtifactDetailAPIView(generics.RetrieveAPIView):
+    """
+    A view that provides detail for a single artifact.
+
+    It extends Django REST Framework's RetrieveAPIView.
+
+    Attributes:
+        queryset: Specifies the queryset that this view will use to retrieve
+            the Artifact object. It retrieves all Artifact objects.
+        serializer_class: Specifies the serializer class that should be used
+            for serializing the Artifact object.
+        permission_classes: Defines the list of permissions that apply to
+            this view. It is set to allow any user to access this view.
+    """
+
     queryset = Artifact.objects.all()
     serializer_class = ArtifactSerializer
     permission_classes = [permissions.AllowAny]
 
 
 class MetadataListAPIView(generics.ListAPIView):
+    """
+    A view that provides a list of metadata related to artifacts.
+
+    It extends Django REST Framework's ListAPIView.
+
+    Attributes:
+        permission_classes: Defines the list of permissions that apply to this
+            view. It is set to allow any user to access this view.
+    """
+
     permission_classes = [permissions.AllowAny]
 
     def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests.
+
+        It retrieves all shapes, tags, and cultures from the database, serializes them,
+        and returns them in a response.
+
+        Args:
+            request: The HTTP request object.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Response: Django REST Framework's Response object containing serialized
+                data for shapes, tags, and cultures.
+        """
         shapes = Shape.objects.all()
         tags = Tag.objects.all()
         cultures = Culture.objects.all()
@@ -74,12 +128,44 @@ class MetadataListAPIView(generics.ListAPIView):
 
 
 class ArtifactDownloadAPIView(generics.RetrieveAPIView, generics.CreateAPIView):
+    """
+    A view that provides downloaded data of detailed information about an artifact.
+
+    Allows the creation of artifact requester records. It extends Django REST
+    Framework's RetrieveAPIView and CreateAPIView.
+
+    Attributes:
+        queryset: Specifies the queryset that this view will use to retrieve
+            the Artifact object. It retrieves all Artifact objects.
+        serializer_class: Specifies the serializer class that should be used
+            for serializing the Artifact object.
+        lookup_field: Specifies the field that should be used to retrieve a
+            single object from the queryset.
+        permission_classes: Defines the list of permissions that apply to
+            this view. It is set to allow any user to access this view.
+    """
+
     queryset = Artifact.objects.all()
     serializer_class = ArtifactSerializer
     lookup_field = "pk"
     permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
+        """
+        Handles POST requests.
+
+        It creates a new artifact requester record and returns a response containing
+        the serialized data for the created artifact requester.
+
+        Args:
+            request: The HTTP request object.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Response: Django REST Framework's Response object containing serialized
+                data for the created artifact requester.
+        """
         logger.info(
             "Creating new artifact requester for artifact {}".format(kwargs.get("pk"))
         )
@@ -119,6 +205,21 @@ class ArtifactDownloadAPIView(generics.RetrieveAPIView, generics.CreateAPIView):
         return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
 
     def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests.
+
+        It retrieves detailed information about an artifact and returns a response
+        containing the serialized data for the artifact.
+
+        Args:
+            request: The HTTP request object.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Response: Django REST Framework's Response object containing serialized
+                data for the artifact.
+        """
         pk = kwargs.get("pk")
         if pk is not None:
             logger.info(f"Downloading artifact {pk}")
@@ -163,9 +264,27 @@ class ArtifactDownloadAPIView(generics.RetrieveAPIView, generics.CreateAPIView):
 
 
 class CustomPageNumberPagination(PageNumberPagination):
+    """
+    A custom pagination class that provides paginated responses for API views.
+
+    It extends Django REST Framework's PageNumberPagination.
+
+    Attributes:
+        page_size: Specifies the number of items to display per page.
+    """
+
     page_size = 9
 
     def get_paginated_response(self, data):
+        """
+        Retrieves paginated response data.
+
+        Args:
+            data: The data to be paginated.
+
+        Returns:
+            Response: Django REST Framework's Response object containing paginated data.
+        """
         return Response(
             {
                 "current_page": int(self.request.query_params.get("page", 1)),
@@ -178,11 +297,31 @@ class CustomPageNumberPagination(PageNumberPagination):
 
 
 class CatalogAPIView(generics.ListAPIView):
+    """
+    A view that provides a list of artifacts in the catalog.
+
+    It extends Django REST Framework's ListAPIView.
+
+    Attributes:
+        serializer_class: Specifies the serializer class that should be used
+            for serializing the Artifact objects.
+        pagination_class: Specifies the pagination class that should be used
+            for paginating the response data.
+        permission_classes: Defines the list of permissions that apply to
+            this view. It is set to allow any user to access this view.
+    """
+
     serializer_class = CatalogSerializer
     pagination_class = CustomPageNumberPagination
     permission_classes = [permissions.AllowAny]
 
     def get_queryset(self):
+        """
+        Retrieves the queryset for the view.
+
+        Returns:
+            queryset: The queryset containing all artifacts in the catalog.
+        """
         queryset = Artifact.objects.all().order_by("id")
 
         # Filter by query parameters
@@ -211,9 +350,30 @@ class CatalogAPIView(generics.ListAPIView):
         return queryset.filter(q_objects)
 
     def get_serializer_context(self):
+        """
+        Retrieves the context for the serializer.
+
+        Returns:
+            dict: A dictionary containing the request object.
+        """
         return {"request": self.request}
 
     def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests.
+
+        It retrieves all artifacts in the catalog, serializes them, and returns
+        paginated data in a response.
+
+        Args:
+            request: The HTTP request object.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Response: Django REST Framework's Response object containing paginated
+                data for the artifacts.
+        """
         queryset = self.filter_queryset(self.get_queryset())
         page = self.paginate_queryset(queryset)
         if page is not None:
@@ -226,6 +386,25 @@ class CatalogAPIView(generics.ListAPIView):
 
 
 class ArtifactCreateUpdateAPIView(generics.GenericAPIView):
+    """
+    A view that provides functionality for creating and updating artifacts.
+
+    It extends Django REST Framework's GenericAPIView.
+
+    Attributes:
+        queryset: Specifies the queryset that this view will use to retrieve
+            the Artifact objects. It retrieves all Artifact objects.
+        serializer_class: Specifies the serializer class that should be used
+            for serializing the Artifact objects.
+        lookup_field: Specifies the field that should be used to retrieve a
+            single object from the queryset.
+        authentication_classes: Defines the list of authentication classes that
+            apply to this view. It is set to TokenAuthentication.
+        permission_classes: Defines the list of permissions that apply to
+            this view. It is set to allow only authenticated users with the
+            role of 'Funcionario' or 'Administrador' to access this view.
+    """
+
     queryset = Artifact.objects.all()
     serializer_class = UpdateArtifactSerializer
     lookup_field = "pk"
@@ -235,21 +414,84 @@ class ArtifactCreateUpdateAPIView(generics.GenericAPIView):
     ]
 
     def get_object(self):
+        """
+        Retrieves the object for the view.
+
+        Returns:
+            object: The object retrieved from the queryset based on the primary key.
+        """
         pk = self.kwargs.get("pk")
         if pk is not None:
             return super().get_object()
         return None
 
     def post(self, request, *args, **kwargs):
+        """
+        Handles POST requests.
+
+        It creates a new artifact and returns a response containing the serialized
+        data for the created artifact.
+
+        Args:
+            request: The HTTP request object.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Response: Django REST Framework's Response object containing serialized
+                data for the created artifact.
+        """
         return self.create_or_update(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
+        """
+        Handles PUT requests.
+
+        It updates an existing artifact and returns a response containing the serialized
+        data for the updated artifact.
+
+        Args:
+            request: The HTTP request object.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Response: Django REST Framework's Response object containing serialized
+                data for the updated artifact.
+        """
         return self.create_or_update(request, *args, **kwargs)
 
     def patch(self, request, *args, **kwargs):
+        """
+        Handles PATCH requests.
+
+        It partially updates an existing artifact and returns a response containing the
+        serialized data for the updated artifact.
+
+        Args:
+            request: The HTTP request object.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Response: Django REST Framework's Response object containing serialized
+                data for the updated artifact.
+        """
         return self.create_or_update(request, *args, **kwargs, partial=True)
 
     def create_or_update(self, request, *args, **kwargs):
+        """
+        Creates or updates an artifact based on the request data.
+
+        Args:
+            request: The HTTP request object.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Response: Django REST Framework's Response object containing serialized
+                data for the created or updated artifact.
+        """
         partial = kwargs.pop("partial", False)
         instance = self.get_object()
 
@@ -282,9 +524,23 @@ class ArtifactCreateUpdateAPIView(generics.GenericAPIView):
         )
 
     def perform_create_or_update(self, serializer):
+        """
+        Performs the creation or update of an artifact based on the serializer.
+
+        Args:
+            serializer: The serializer object that contains the data for the artifact.
+        """
         serializer.save()
 
     def handle_file_uploads(self, instance, files, data):
+        """
+        Handles file uploads for an artifact.
+
+        Args:
+            instance: The Artifact object for which the file uploads are being handled.
+            files: The files to be uploaded.
+            data: The data associated with the files.
+        """
         # Handle thumbnail
         thumbnail_data = files.get("new_thumbnail")
         if thumbnail_data:
@@ -390,11 +646,39 @@ class ArtifactCreateUpdateAPIView(generics.GenericAPIView):
 
 
 class InstitutionAPIView(generics.ListCreateAPIView):
+    """
+    A view that provides a list of institutions.
+
+    It extends Django REST Framework's ListCreateAPIView.
+
+    Attributes:
+        queryset: Specifies the queryset that this view will use to retrieve
+            the Institution objects. It retrieves all Institution objects.
+        serializer_class: Specifies the serializer class that should be used
+            for serializing the Institution objects.
+        permission_classes: Defines the list of permissions that apply to
+            this view. It is set to allow any user to access this view.
+    """
+
     queryset = Institution.objects.all().order_by("id")
     serializer_class = InstitutionSerializer
     permission_classes = [permissions.AllowAny]
 
     def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests.
+
+        It retrieves all institutions, serializes them, and returns them in a response.
+
+        Args:
+            request: The HTTP request object.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Response: Django REST Framework's Response object containing serialized
+                data for the institutions.
+        """
         institutions = Institution.objects.all()
 
         # Serialize the data
