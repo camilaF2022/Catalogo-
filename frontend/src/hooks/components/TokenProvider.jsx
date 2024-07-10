385ab7959fa00e0e07e483fc00902fff9b69252b
paulacalderona <70768936+paulacalderona@users.noreply.github.com>
2024-07-10 13:34:22 -0400

    adding more files

--- frontend/src/hooks/components/TokenProvider.jsx
+++ frontend/src/hooks/components/TokenProvider.jsx
@@ -2,14 +2,19 @@
 
 import React, { createContext, useState, useCallback, useMemo } from "react";
 
+// Create a new context to manage authentication token state
 export const TokenContext = createContext();
 
+// TokenProvider component manages the state of the authentication token
 export function TokenProvider({ children }) {
+
+// Initialize token state, retrieving it from localStorage if available
   const [token, setTokenState] = useState(() => {
     const tokenString = localStorage.getItem("token");
     return tokenString ?? null;
   });
 
+// Function to update the token state and store it in localStorage
   const setToken = useCallback((newToken) => {
     if (newToken) {
       localStorage.setItem("token", JSON.stringify(newToken));
@@ -19,6 +24,7 @@ export function TokenProvider({ children }) {
     setTokenState(newToken);
   }, []);
 
+ // Memoize the context value to optimize performance
   const contextValue = useMemo(
     () => ({
       token,
@@ -27,6 +33,7 @@ export function TokenProvider({ children }) {
     [token, setToken]
   );
 
+// Provide the TokenContext.Provider with the context value to its children
   return (
     <TokenContext.Provider value={contextValue}>
       {children}
