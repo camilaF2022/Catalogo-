f4830fd9027f5a7e2eb8fd93e09c9f4e884efcdc
Katia Fredes <katia.fredes.a@gmail.com>
2024-07-09 20:08:39 -0400

    docs: add dosctring and documentation

--- backend/catalogo_arqueologico/catalogo_arqueologico/views.py
+++ backend/catalogo_arqueologico/catalogo_arqueologico/views.py
@@ -1,3 +1,11 @@
+"""
+This module contains views for the user authentication system.
+
+It includes the LoginView class, which provides an API endpoint for user login. The LoginView
+handles POST requests, authenticating users based on email and password, and returns a token
+for authenticated sessions.
+"""
+
 from piezas.models import CustomUser
 from .serializer import UserSerializer
 from rest_framework import status
@@ -7,7 +15,36 @@ from rest_framework.authtoken.models import Token
 
 
 class LoginView(APIView):
+    """
+    API View for user login.
+
+    This view handles POST requests to authenticate users. It expects an email and password
+    in the request data, validates them, and returns a token for authenticated sessions along
+    with user data if the credentials are valid.
+
+    Methods:
+        post(request): Handles the POST request to authenticate a user.
+            - Parameters:
+                - request: HttpRequest object containing the request data.
+            - Returns:
+                - Response object with token and user data if authentication is successful.
+                - Response object with error detail if authentication fails.
+    """
+
     def post(self, request):
+        """
+        Authenticate a user based on email and password.
+
+        This method checks if the request data includes 'email' and 'password'.
+
+        Parameters:
+            request (HttpRequest): The request object containing the email and password.
+
+        Returns:
+            Response: A DRF Response object with the authentication token and user data if the login
+            is successful, or an error message if the login fails.
+        """
+
         if "email" not in self.request.data or "password" not in self.request.data:
             return Response(
                 {"detail": "Ingrese correo y contraseña"},
