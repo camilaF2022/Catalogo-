e8b3aaa5279f004dbffca3ebadf7850cf3131fa7
Katia Fredes <katia.fredes.a@gmail.com>
2024-07-09 20:22:20 -0400

    style: change formatting ' to " according to black formatter

--- backend/catalogo_arqueologico/piezas/apps.py
+++ backend/catalogo_arqueologico/piezas/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PiezasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'piezas'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "piezas"
